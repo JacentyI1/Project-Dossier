b7ac56ed2272ec3aadbebecc1329c80e25c260b5
Jacek K. <jacekkwiecinski99@gmail.com>
2024-02-08 15:30:26 +0100

    Email service configuration

---
+++ src/main/java/com/jack/projectdossier/config/MailConfig.java
@@ -0,0 +1,33 @@
+package com.jack.projectdossier.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    private final Integer port = Integer.parseInt("${spring.mail.port}");
+    private final String username = "${spring.mail.username}";
+    private final String password = "${spring.mail.password}";
+    private final String host = "${spring.mail.host}";
+    private final String protocol = "${spring.mail.protocol}";
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", protocol);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
