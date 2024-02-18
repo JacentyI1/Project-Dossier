e1e0c869cfc5cedf89f2eb8dd3c1c967a8e77d52
Jacek K. <jacekkwiecinski99@gmail.com>
2024-02-18 21:44:48 +0100

    Mail config fix

--- src/main/java/com/jack/projectdossier/config/MailConfig.java
+++ src/main/java/com/jack/projectdossier/config/MailConfig.java
@@ -9,21 +9,16 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
-    private final Integer port = Integer.parseInt("${spring.mail.port}");
-    private final String username = "${spring.mail.username}";
-    private final String password = "${spring.mail.password}";
-    private final String host = "${spring.mail.host}";
-    private final String protocol = "${spring.mail.protocol}";
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+        mailSender.setHost("${spring.mail.host}");
+        mailSender.setPort(587);
+        mailSender.setUsername("${spring.mail.username}");
+        mailSender.setPassword("${spring.mail.password}");
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", protocol);
+        props.put("mail.transport.protocol", "${spring.mail.protocol}");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
