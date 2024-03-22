9a86899021f5c098e41c25bb29a6e514f6748ddb
JacentyI1 <mail.jacek.kwiecinski@gmail.com>
2024-03-22 15:24:25 +0100

    Improved readability

--- src/main/java/com/jack/projectdossier/repositories/TokenRepository.java
+++ src/main/java/com/jack/projectdossier/repositories/TokenRepository.java
@@ -12,9 +12,9 @@ public interface TokenRepository  extends JpaRepository<TokenEntity, Long> {
 
     @Query(
            value = """
-                select t from TokenEntity t inner join UserEntity u\s
-                on t.user.id = u.id\s
-                where u.id = :id and (t.revoked = false and t.expired = false )\s
+                SELECT t FROM TokenEntity t INNER JOIN UserEntity u\s
+                ON t.user.id = u.id\s
+                WHERE u.id = :id AND (t.revoked = FALSE AND t.expired = FALSE )\s
             """
     )
     List<TokenEntity> findAllValidTokenByCustomer(Long id);
