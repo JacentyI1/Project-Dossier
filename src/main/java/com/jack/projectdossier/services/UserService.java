789d3caa36a1676b781d3c6fbded9786c019052a
JacentyI1 <mail.jacek.kwiecinski@gmail.com>
2024-03-24 11:36:07 +0100

    UserService : userCreate() Encode user's password with Bcrypt, and save it with userRepository (into database). @return created user with encoded password

--- src/main/java/com/jack/projectdossier/services/UserService.java
+++ src/main/java/com/jack/projectdossier/services/UserService.java
@@ -1,4 +1,10 @@
 package com.jack.projectdossier.services;
 
+import com.jack.projectdossier.dao.entities.UserEntity;
+
 public interface UserService {
+//    UserEntity save
+//    public UserEntity save(UserEntity userEntity);
+
+    UserEntity createUser(UserEntity userEntity);
 }
--- src/main/java/com/jack/projectdossier/services/impl/UserServiceImpl.java
+++ src/main/java/com/jack/projectdossier/services/impl/UserServiceImpl.java
@@ -1,6 +1,32 @@
 package com.jack.projectdossier.services.impl;
 
+import com.jack.projectdossier.dao.entities.UserEntity;
+import com.jack.projectdossier.repositories.UserRepository;
 import com.jack.projectdossier.services.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+@Log
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserEntity createUser(UserEntity userEntity) {
+        // encode password
+        String encodedPassword = passwordEncoder.encode(userEntity.getPassword());
+        // set password parameter
+        userEntity.setPassword(encodedPassword);
+        // save user in the repository
+        userRepository.save(userEntity);
+        // return saved user for debugging (maybe something else)
+        return userEntity;
+    }
+
+
 }
